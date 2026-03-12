e671c890334bf68ee2a5173cab00216d5ae9ffff
Oriana <98231498+OrianaLand@users.noreply.github.com>
2026-03-12 14:26:32 -0300

    Create dedicated classes/ and tests/ directories for better porject structure

--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -1,5 +1,5 @@
-import { Gameboard } from "./gameboard";
-import { Ship } from "./ship";
+import { Gameboard } from "../gameboard";
+import { Ship } from "../classes/ship";
 describe("Gameboard", () => {
   let gameboard;
 
