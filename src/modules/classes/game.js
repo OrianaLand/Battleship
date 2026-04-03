f98114aeb0b4bfd7a0665458f2b68fce89414a8d
Oriana <98231498+OrianaLand@users.noreply.github.com>
2026-04-03 13:32:21 -0300

    fix: fix ternary condition for random orientation generation

--- src/modules/classes/game.js
+++ src/modules/classes/game.js
@@ -15,4 +15,20 @@ export class Game {
     if (this.state !== "setup") throw new Error("Game already started");
     return this.human.gameboard.placeShip(ship, row, col, orientation);
   }
+
+  placeCPUShipsRandomly() {
+    const shipLengths = [5, 4, 3, 3, 2];
+
+    for (const lenght of shipLengths) {
+      const ship = new Ship(lenght);
+      let placed = false;
+
+      while (!placed) {
+        const row = Math.floor(Math.random() * this.cpu.gameboard.size);
+        const col = Math.floor(Math.random() * this.cpu.gameboard.size);
+        const orientation = Math.random() < 0.5 ? "H" : "V";
+        placed = this.cpu.gameboard.placeShip(ship, row, col, orientation);
+      }
+    }
+  }
 }
