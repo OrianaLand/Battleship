13a51bd29808ca355684d70db9c1d6fa949dd196
Oriana <98231498+OrianaLand@users.noreply.github.com>
2026-04-17 19:12:53 -0300

    feat(controller): add game initialization and board rendering)

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,15 +1,30 @@
 import { createBoardGrid, updateCell } from "../dom/renderBoard";
-export class gameController{
-    constructor(game){
-        this.game = game;
-        this.views = [];
-    }
+import { Game } from "../modules/classes/game";
+import { Ship } from "../modules/classes/ship";
+export class GameController {
+  constructor() {
+    this.game = new Game();
+    this.humanGrid = null;
+    this.cpuGrid = null;
+    this.views = [];
+  }
 
-    // init two board
+  // initialize
+  init() {
+    this.game.placeHumanShip(new Ship(5), 0, 0, "H");
+    this.game.placeHumanShip(new Ship(4), 2, 0, "V");
+    this.game.placeHumanShip(new Ship(3), 4, 5, "V");
+    this.game.placeHumanShip(new Ship(3), 8, 2, "H");
+    this.game.placeHumanShip(new Ship(2), 2, 8, "H");
 
-    initBoards(this.game){
-        createBoardGrid(this.game.human.gameBoard);
-        createBoardGrid(this.game.cpu.gameBoard);
-    }
-    //handle human attack on cpu grid
+    this.game.startGame();
+
+    const humanContainer = document.querySelector("#human-board");
+    const cpuContainer = document.querySelector("#cpu-board");
+
+    this.humanGrid = createBoardGrid(this.game.human.gameboard, humanContainer);
+    this.cpuGrid = createBoardGrid(this.game.cpu.gameboard, cpuContainer);
+  }
+
+  //handle human attack on cpu grid
 }
