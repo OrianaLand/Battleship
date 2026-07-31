8bca897859752d61d0eb3dbeb6497b18ed7829ce
Oriana <98231498+OrianaLand@users.noreply.github.com>
2026-07-31 20:03:46 -0300

    chore(controller): remove debug logs, dead code and unused imports

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,8 +1,5 @@
-import { createBoardGrid, updateCell } from "../dom/renderBoard";
 import { Game } from "../modules/classes/game";
 import { Ship } from "../modules/classes/ship";
-import { StatusView } from "../views/statusView";
-import { GameBoardView } from "../views/GameBoardView";
 import { GameView } from "../views/gameView";
 import { SetupView } from "../views/SetupView";
 
@@ -18,7 +15,6 @@ export class GameController {
   // initialize
   init() {
     this.#setupPhase();
-    console.log("called init???");
   }
 
   #setupPhase() {
@@ -43,11 +39,9 @@ export class GameController {
   this.setupView = new SetupView(humanBoardContainer, onShipPlaced, onComplete);
 
   this.setupView.render(this.game.human.gameboard, ships);
-    console.log("called setupPhase");
   }
 
   #startGame() {
-    console.log("called startgame");
     this.game.startGame();
     this.#initViews();
   }
@@ -57,19 +51,6 @@ export class GameController {
       this.game.human.gameboard,
       this.game.cpu.gameboard,
     );
-    console.log("called gameView");
-  }
-
-  //handle human attack on cpu grid
-  #attachCPUBoardListener() {
-    this.cpuGrid.addEventListener("click", (e) => {
-      const cell = e.target.closest(".cell");
-
-      const row = parseInt(cell.dataset.row);
-      const col = parseInt(cell.dataset.col);
-
-      this.#handleHumanAttack(row, col);
-    });
   }
 
   #handleHumanAttack(row, col) {
@@ -82,11 +63,8 @@ export class GameController {
       this.gameView.updateCpuBoard(row, col);
       this.gameView.setMessage(`You attacked (${row}, ${col}): ${result}`);
       this.gameView.setTurn(`Current turn: ${this.game.currentTurn}`);
-      console.log(`Human attacked (${row}, ${col}): ${result}`);
-      console.log(`Current turn: ${this.game.currentTurn}`);
 
       if (this.game.state === "over") {
-        console.log(`Game over! Winner: ${this.game.winner}`);
         this.gameView.showGameOver(this.game.winner);
         return;
       }
@@ -112,11 +90,8 @@ export class GameController {
     this.gameView.setMessage(`CPU attacked (${row}, ${col}): ${result}`);
     this.gameView.setTurn(`Current turn: ${this.game.currentTurn}`);
 
-    console.log(`CPU attacked (${row}, ${col}): ${result}`);
-    console.log(`Current turn: ${this.game.currentTurn}`);
 
     if (this.game.state === "over") {
-      console.log(`Game over! Winner: ${this.game.winner}`);
 
       this.gameView.showGameOver(this.game.winner);
       return;
